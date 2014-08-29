a795c84db89b66a7c7d1e52b51d663bd6f0054ec
unknown <saon@697-334-Bard.sf.k12.internal>
2014-08-29 11:43:03 -0700

    Changed Original Design

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,50 @@
-void setup()
-{
 
-}
-void draw()
-{
-  ellipse(50,50,50,50);
+void setup() {
+	size(300,350);
+	background(255);
 }
 
+void draw() {
+  racket();
+  strings();
+  frame();
+  grip();
+}
+
+void racket() {
+	fill(173,255,47);
+	strokeWeight(1);
+	ellipse(150,140,170,200);
+}
+
+void strings()
+{
+	strokeWeight(2);
+	stroke(255);
+	int y =60;
+	while(y < 230)
+	{
+		line(50, y, 250, y);
+		y = y + 10;
+	}
+	int  x=80;
+	while(x < 230)
+	{
+		line(x, 30, x, 230);
+		x = x + 10;
+	}
+	
+}
+
+void frame(){
+	noFill();
+	strokeWeight(8);
+	stroke(10);
+	ellipse(150,140,170,200);
+}
+
+void grip() {
+	stroke(51);
+	strokeWeight(20);
+  	line(150,250,150,350);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,15 +14,56 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
-public void setup()
-{
 
-}
-public void draw()
-{
-  ellipse(50,50,50,50);
+public void setup() {
+	size(300,350);
+	background(255);
 }
 
+public void draw() {
+  racket();
+  strings();
+  frame();
+  grip();
+}
+
+public void racket() {
+	fill(173,255,47);
+	strokeWeight(1);
+	ellipse(150,140,170,200);
+}
+
+public void strings()
+{
+	strokeWeight(2);
+	stroke(255);
+	int y =60;
+	while(y < 230)
+	{
+		line(50, y, 250, y);
+		y = y + 10;
+	}
+	int  x=80;
+	while(x < 230)
+	{
+		line(x, 30, x, 230);
+		x = x + 10;
+	}
+	
+}
+
+public void frame(){
+	noFill();
+	strokeWeight(8);
+	stroke(10);
+	ellipse(150,140,170,200);
+}
+
+public void grip() {
+	stroke(51);
+	strokeWeight(20);
+  	line(150,250,150,350);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
